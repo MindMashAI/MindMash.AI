058d9abd0e01e01875efe7cc2af14ae0806838b3
MindMashAI <info@mindmash.ai>
2025-02-28 07:01:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -556,4 +556,4 @@ def terms():
     return render_template("terms.html", beta_mode=beta_mode)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="127.0.0.1", port=5000)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
