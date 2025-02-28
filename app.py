c24e830a4825dfb668d75baa1ff0f7b64864fb8a
MindMashAI <info@mindmash.ai>
2025-02-28 07:06:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -556,4 +556,6 @@ def terms():
     return render_template("terms.html", beta_mode=beta_mode)
 
 if __name__ == "__main__":
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
     socketio.run(app, debug=True, host="0.0.0.0", port=5000)
